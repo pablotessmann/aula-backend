5863a166de84e5a85a8402a96a67d59504e8a6b6
Pablo Tessmann <p25.tessmann@gmail.com>
2026-07-15 19:12:56 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite seu nome: ");
+        string nome = Console.ReadLine();
+
+        Console.Write("Digite sua idade: ");
+        int idade = int.Parse(Console.ReadLine());
+
+        if (idade <= 20)
+        {
+            Console.WriteLine($"Você é novo. Você tem a idade de {idade}!");
+        } else
+        {
+            Console.WriteLine($"Você é veio! Esta com idade de {idade}!");
+        }
+
+        Console.WriteLine($"Olá {nome}!");
+        Console.WriteLine($"Você possui {idade} anos.");
+    }
+}
